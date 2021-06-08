129da277205f3b1466142e51f223bf4d9adfde2e
VictoriaPlis <victoriyanew@gmail.com>
2021-06-08 14:16:38 +0300

    hw-8

---
+++ gallery-modal.js
@@ -0,0 +1,85 @@
+import galleryItems from "./gallery-items.js";
+
+const galleryRef = document.querySelector(".js-gallery");
+const modalLightboxRef = document.querySelector(".js-lightbox");
+const lightboxImageRef = modalLightboxRef.querySelector(".lightbox__image");
+
+galleryRef.addEventListener("click", onModalLightboxOpen);
+modalLightboxRef.addEventListener("click", onModalLightboxClose);
+
+function makeGalleryItemMarkup(items) {
+  return items.map(({ original, preview, description }) => {
+    const galleryItemEl = document.createElement("li");
+    galleryItemEl.classList.add("gallery__item");
+
+    const galleryLinkEl = document.createElement("link");
+    galleryLinkEl.classList.add("gallery__link");
+    galleryLinkEl.href = original;
+
+    const galleryImageEl = document.createElement("img");
+    galleryImageEl.classList.add("gallery__image");
+    galleryImageEl.src = preview;
+    galleryImageEl.setAttribute("data-source", original);
+    galleryImageEl.alt = description;
+
+    galleryLinkEl.appendChild(galleryImageEl);
+    galleryItemEl.appendChild(galleryLinkEl);
+
+    return galleryItemEl;
+  });
+}
+
+function makeGalleryItemsEls(items) {
+  const galleryItemsEls = makeGalleryItemMarkup(items);
+  galleryRef.append(...galleryItemsEls);
+}
+
+makeGalleryItemsEls(galleryItems);
+
+function onModalLightboxOpen(event) {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery__image")) {
+    modalLightboxRef.classList.add("is-open");
+    lightboxImageRef.src = event.target.dataset.source;
+    lightboxImageRef.alt = event.target.alt;
+  }
+
+  window.addEventListener("keydown", onModalLightboxClose);
+  window.addEventListener("keydown", switchImagesWithArrowKeys);
+}
+
+function onModalLightboxClose(event) {
+  if (
+    event.target.dataset.action === "close-lightbox" ||
+    event.target.classList.contains("lightbox__overlay") ||
+    event.key === "Escape"
+  ) {
+    modalLightboxRef.classList.remove("is-open");
+    lightboxImageRef.src = "";
+    lightboxImageRef.alt = "";
+    window.removeEventListener("keydown", onModalLightboxClose);
+    window.removeEventListener("keydown", switchImagesWithArrowKeys);
+  }
+}
+
+function switchImagesWithArrowKeys(event) {
+  let imageIndex = galleryItems.findIndex(
+    (item) => item.original === lightboxImageRef.src
+  );
+
+  if (event.code === "ArrowLeft") {
+    imageIndex === 0
+      ? (imageIndex = galleryItems.length - 1)
+      : (imageIndex -= 1);
+  }
+
+  if (event.code === "ArrowRight") {
+    imageIndex === galleryItems.length - 1
+      ? (imageIndex = 0)
+      : (imageIndex += 1);
+  }
+
+  lightboxImageRef.src = galleryItems[imageIndex].original;
+  lightboxImageRef.alt = galleryItems[imageIndex].description;
+}
